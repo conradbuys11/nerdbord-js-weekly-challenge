24a5fa1ed69dcd5eee8099bb573589220716cf97
conradbuys11 <conradbuys11@gmail.com>
2023-06-02 12:11:00 -0400

    feat: added images, css structure
    just the skeleton rn, more work to be done

---
+++ challenges/week-4/app.js
@@ -0,0 +1,7 @@
+// TO-DO:
+// - get list of plant cards
+// - attach the links and the text to the css classes somehow? unsure about this one
+// - either that or set up the hover/focus code manually in js, which seems a little wack
+
+// SIKE I'VE BEEN DOING THIS WRONG ALL ALONG
+// LET'S MAKE THE STUFF IN JS
