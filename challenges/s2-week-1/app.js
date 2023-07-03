1d0fcd157a4f6f5ba7821632bace7ab2e233fb65
conradbuys11 <conradbuys11@gmail.com>
2023-07-03 13:07:19 -0400

    feat: added hands, no functionality yet
    just made a test function, need to fix positioning before adding more functionality

---
+++ challenges/s2-week-1/app.js
@@ -0,0 +1,15 @@
+const clock = document.querySelector("div.clock-outer-circle");
+let curRotation = 0;
+const minHand = document.querySelector("div.minute-hand");
+
+const rotateMinHand = (rotateAmount) => {
+  const amount = typeof rotateAmount === "number" ? rotateAmount : 5;
+  curRotation =
+    curRotation + amount >= 360
+      ? curRotation + amount - 360
+      : curRotation + amount;
+  minHand.style.transform = `rotate(${curRotation}deg)`;
+};
+
+console.log(clock);
+// document.addEventListener("keypress", (e) => rotateMinHand(10));
