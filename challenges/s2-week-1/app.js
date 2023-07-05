4a6c07ff2531471164e33817792d50abde9de863
conradbuys11 <conradbuys11@gmail.com>
2023-07-05 18:05:17 -0400

    feat: millisecs added, scale increased
    also made hands larger, and made functionality fire 20 times a sec

--- challenges/s2-week-1/app.js
+++ challenges/s2-week-1/app.js
@@ -11,9 +11,10 @@ const parseCurrentTime = () => {
   const curHours = hour >= 12 ? hour - 12 : hour;
   const curMinutes = _date.getMinutes();
   const curSeconds = _date.getSeconds();
+  const curMilliseconds = _date.getMilliseconds();
   rotateHourHand(curHours, curMinutes);
   rotateMinHand(curMinutes, curSeconds);
-  rotateSecHand(curSeconds);
+  rotateSecHand(curSeconds, curMilliseconds);
 };
 
 const rotateHand = (rotateAmount, handObj) => {
@@ -22,27 +23,18 @@ const rotateHand = (rotateAmount, handObj) => {
 
 const rotateHourHand = (curHours, curMinutes) => {
   const rotateAmount = (curHours + curMinutes / 60) * 30;
-  console.log(
-    `rotating hour hand: ${curHours}:${curMinutes} is a rotation of ${rotateAmount} degrees`
-  );
   rotateHand(rotateAmount, hourHand);
 };
 
 const rotateMinHand = (curMinutes, curSeconds) => {
   const rotateAmount = (curMinutes + curSeconds / 60) * 6;
-  console.log(
-    `rotating minute hand: x:${curMinutes}:${curSeconds} is a rotation of ${rotateAmount} degrees`
-  );
   rotateHand(rotateAmount, minHand);
 };
 
-const rotateSecHand = (curSeconds) => {
-  const rotateAmount = curSeconds * 6;
-  console.log(
-    `rotating second hand: x:x:${curSeconds} is a rotation of ${rotateAmount} degrees`
-  );
+const rotateSecHand = (curSeconds, curMilliseconds) => {
+  const rotateAmount = (curSeconds + curMilliseconds / 1000) * 6;
   rotateHand(rotateAmount, secondHand);
 };
 
 parseCurrentTime();
-setInterval(parseCurrentTime, 1000);
+setInterval(parseCurrentTime, 50);
