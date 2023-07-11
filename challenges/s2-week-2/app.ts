4078118983e88ffef4ec7086e2e0e7b7567026dc
conradbuys11 <conradbuys11@gmail.com>
2023-07-11 11:24:23 -0400

    feat: added imgs

--- challenges/s2-week-2/app.ts
+++ challenges/s2-week-2/app.ts
@@ -1,24 +1,30 @@
 const mainBox = document.querySelector("div.main-box")!;
 
-// const createAllTextBoxes = (arrOfTextBoxes) => {};
+type TextBoxObject = {
+  num: number | string;
+  text: string;
+  picture: any;
+  location: any;
+};
 
-const createTextBox = (
-  num: number | string,
-  text: string,
-  picture: any,
-  location: any
-) => {
+const createAllTextBoxes = (arrOfTextBoxes: TextBoxObject[]) => {
+  arrOfTextBoxes.forEach((textBoxObj) => {
+    createTextBox(textBoxObj);
+  });
+};
+
+const createTextBox = (textBoxObj: TextBoxObject) => {
   let textBox = document.createElement("div");
   textBox.classList.add("text-box");
 
   let textNum = document.createElement("span");
   textNum.classList.add("text", "text-number");
-  textNum.innerText = num.toString();
+  textNum.innerText = textBoxObj.num.toString();
   textBox.appendChild(textNum);
 
   let textMain = document.createElement("span");
   textMain.classList.add("text", "text-main");
-  textMain.innerText = text.toUpperCase();
+  textMain.innerText = textBoxObj.text.toUpperCase();
   textBox.appendChild(textMain);
 
   // TO-DO: add functionality for eventlistener, picture
@@ -26,4 +32,14 @@ const createTextBox = (
   mainBox.appendChild(textBox);
 };
 
-createTextBox("02", "brand identity", null, null);
+// TO-DO: populate rest of array
+const textBoxes = [
+  { num: "01", text: "interiors", picture: "interiors", location: null },
+];
+
+createTextBox({
+  num: "02",
+  text: "brand identity",
+  picture: null,
+  location: null,
+});
